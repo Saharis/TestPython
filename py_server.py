ec927e8a46e23c74300cdb906268ca1b41adf3f0
unknown <liuwj@DST59931.cn1.global.ctrip.com>
2015-03-04 18:27:47 +0800

    sync

--- py_server.py
+++ py_server.py
@@ -8,11 +8,11 @@ s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((HOST,PORT))
 s.listen(3)
 print('LOG |||| listenning ')
-coon,addr=s.accept()
+conn,addr=s.accept()
 
 request=conn.recv(1024)
 
 print('LOG |||| request is ',request,end='\n')
 print('LOG |||| Connected by ',addr,end='\n')
-conn.sendall(reply)
+conn.sendall(reply.encode)
 conn.close
