da4c435893f83b694833266b412f2d9eff3c3e81
liuwj <liuwj@ctrip.com>
2015-03-12 16:48:35 +0800

    sync

--- py_client.py
+++ py_client.py
@@ -3,12 +3,12 @@ import socket
 HOST='172.16.150.49'
 PORT=8000
 
-request='my requset info'
+request=b'my requset info'
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print 'LOG |||| start connetting '
+print('LOG |||| start connetting')
 s.connect((HOST,PORT))
 
 s.sendall(request)
 reply=s.recv(1024)
-print 'LOG |||| reply is ',reply.decode
+print('LOG |||| reply is ',reply.decode('UTF-8'))
 s.close
